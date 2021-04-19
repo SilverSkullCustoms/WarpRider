4e81590ea35d8fc0a3ed14c2e382cc71cd0f24e7
Lukey2501 <81759397+Lukey2501@users.noreply.github.com>
2021-04-19 11:26:45 +0200

    Rotation script added
    rotation script added to sphere

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -2,23 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotationscript : MonoBehaviour
+public class MovementScript : MonoBehaviour
 {
- 
-    public Vector3 rot;
-    public Transform rotcap;
-
     // Start is called before the first frame update
     void Start()
     {
-
-
+        
     }
+
+    // Update is called once per frame
     void Update()
     {
-        rotcap.Rotate(rot);
+        
     }
-
-    
 }
-
