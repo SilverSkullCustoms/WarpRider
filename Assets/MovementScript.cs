653fbcb9df6859c7253d748ca99793e6a35263a5
Lukey2501 <81759397+Lukey2501@users.noreply.github.com>
2021-04-26 11:02:04 +0200

    programming Luke
    we now have the ship and rot script all done with basic movement script

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class MovementScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    Vector2 move;
+    public float speed;
+    public float h;
+    public float v;
+    
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        h = Input.GetAxis("Horizontal");
+
+
+
+        move.x = -h;
+
+        transform.Translate(move * speed);
+
     }
 }
+
--- Assets/Rotationscript.cs
+++ Assets/Rotationscript.cs
@@ -7,16 +7,19 @@ public class Rotationscript : MonoBehaviour
  
     public Vector3 rot;
     public Transform rotcap;
+    public float rotspeed;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rotspeed = 0.015f;
+       
 
     }
     void Update()
     {
         rotcap.Rotate(rot);
+        rot = new Vector3(rotspeed, 0, 0);
     }
 
     
