92617946acd663ca6b5f787af61eac324ffbb06d
justifrusti <70446225+justifrusti@users.noreply.github.com>
2021-06-06 20:55:25 +0200

    samen met shane aan movement gewerkt(nog niet af)

--- Assets/Shane/PlayerController.cs
+++ Assets/Shane/PlayerController.cs
@@ -5,30 +5,51 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
-    public int[] arrayMove;
+
+    public Transform[] arrayMove;
+
+    public Transform nextPos;
+
+    public int index;
     //Array aanmaken met 01234 met daarin Vector3, zodat hij die punten kan pakken, voorloop weghalen
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        nextPos = arrayMove[2];
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
+        /*float h = Input.GetAxis("Horizontal");
 
         Vector3 move = new Vector3();
 
         move.x = -h;
 
+        
         transform.Translate(move * moveSpeed * Time.deltaTime);
+        */
 
-
-        void OnTriggerEnter()
+        if (Input.GetButtonDown("Left"))
         {
-            
+            if (index < arrayMove.Length)
+            {
+                nextPos = arrayMove[index];
+                index--;
+                transform.position = nextPos.position;
+            }
+        }
+
+        if (Input.GetButtonDown("Right"))
+        {
+            if (index < arrayMove.Length)
+            {
+                nextPos = arrayMove[index];
+                index++;
+                transform.position = nextPos.position;
+            }
         }
     }
 }
