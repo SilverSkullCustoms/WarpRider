a931a4eac8419480735adb03ffc62516c7fd264e
SilverSkullCustoms <70266927+SilverSkullCustoms@users.noreply.github.com>
2021-06-21 10:45:18 +0200

    Build scenes added + scenechange is working

--- Assets/Shane/Scripts Player/DeathScript.cs
+++ Assets/Shane/Scripts Player/DeathScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathScript : MonoBehaviour
 {
@@ -21,5 +22,9 @@ public class DeathScript : MonoBehaviour
     {
         deathExplosion.Play();
 
+    }
+    void GameOverScreen()
+    {
+        SceneManager.LoadScene(sceneName: "GameOver");
     }
 }
