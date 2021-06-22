39078f08cc153d868793639a2bbd8f2171ae2ee9
m-Mim-m <81236073+m-Mim-m@users.noreply.github.com>
2021-06-22 13:44:06 +0200

    Menu Buttons work (hopefully)

--- Assets/Isabel/Menu.cs
+++ Assets/Isabel/Menu.cs
@@ -6,21 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    public GameObject startScreen;
-    public GameObject gameOverScreen;
-    public bool alive;
-    /// start
-    public GameObject startA;
-    public GameObject quitA;
-    /// GameOver
-    public GameObject retryG;
-    public GameObject backA;
-
     // Start is called before the first frame update
     void Start()
     {
-        alive = true;
-
 
     }
 
@@ -30,53 +18,24 @@ public class Menu : MonoBehaviour
 
     }
 
-    void QuitGame()
+    public void QuitGame()
     {
         Application.Quit();
     }
 
-    public void StartGame()
+    public void BackToMainMenu()
     {
-        alive = true;
-        startScreen.SetActive(false);
-        gameOverScreen.SetActive(false);
-    }
-
-    ///Buttons
-    public void DisablePanels()
-    {
-        startA.SetActive(false);
-        quitA.SetActive(false);
-        retryG.SetActive(false);
-        backA.SetActive(false);
+        SceneManager.LoadScene(sceneName: "MainMenu");
     }
 
     public void OnStart()
     {
-        DisablePanels();
-        startA.SetActive(true);
         SceneManager.LoadScene(sceneName: "Game");
     }
 
-    public void Quit()
-    {
-        DisablePanels();
-        quitA.SetActive(true);
-    }
-
-    /// GameOver
     public void RetryGameOver()
     {
-        DisablePanels();
-        retryG.SetActive(true);
         SceneManager.LoadScene(sceneName: "Game");
     }
-
-    public void Back()
-    {
-        DisablePanels();
-        startScreen.SetActive(true);
-    }
-
 
 }
